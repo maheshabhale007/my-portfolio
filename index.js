3ae0afd3041e8a1439637396f0906277f71f2c93
Mahesh Abhale <abhale.mahesh@iitgn.ac.in>
2023-03-25 17:49:06 +0530

    left-header done

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const sectBtns = document.querySelectorAll(".control");
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
+// added a function for transitioning between different pages
 function PageTransition () {
-    for (let i = 0; i < sectBtns.length; i++) {
-        sectBtns[i].addEventListener("click", function () {
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener("click", function () {
             let currentBtn = document.querySelectorAll(".active-btn");
             currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
             this.className += " active-btn";
-            console.log("hello");
+            // console.log("hello");
         })
     }
 }
+
+
+allSections.addEventListener('click', (event) => {
+    const id = event.target.dataset.id;
+    if (id) {
+        // removing active-btn 
+        sectBtn.forEach ((btn) => {
+            btn.classList.remove('active');
+        })
+        event.target.classList.add('active');
+
+        // hiding other sections
+        sections.forEach ((section) => {
+            section.classList.remove('active');
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+})
 
 PageTransition();
