9e5130310c8cebe4c28567925d0bf2f688ce04fa
Mahesh Abhale <abhale.mahesh@iitgn.ac.in>
2023-03-29 18:05:23 +0530

    light-mode done

--- index.js
+++ index.js
@@ -35,4 +35,12 @@ allSections.addEventListener('click', (event) => {
     }
 })
 
+// toggle theme
+const themeBtn = document.querySelector('.theme-btn');
+themeBtn.addEventListener('click', () => {
+    let element = document.body;
+    element.classList.toggle('light-mode');
+})
+
+
 PageTransition();
